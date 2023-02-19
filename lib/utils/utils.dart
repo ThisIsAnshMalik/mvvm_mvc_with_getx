5d59ccfbd21ed49697bed0a8787e63e0f509b1e5
Ansh Malik <anshmalik279@gmail.com>
2023-02-19 14:00:32 +0530

    snacbar and toast message utils are added

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 
 class Utils {
   // function to change the focus from the current textfield to another with keyboard
@@ -6,5 +8,16 @@ class Utils {
       BuildContext context, FocusNode current, FocusNode nextFocus) {
     current.unfocus();
     FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  // toast message util
+  static toastMessage(String msg) {
+    Fluttertoast.showToast(
+        msg: msg, backgroundColor: Colors.black, gravity: ToastGravity.BOTTOM);
+  }
+
+  //snack bar with getX
+  static snackBar(String title, String message) {
+    Get.snackbar(title, message);
   }
 }
