ffd69baf75e9c737a12d8c0f15f0e9e65510fb56
Ansh Malik <anshmalik279@gmail.com>
2023-02-19 13:51:57 +0530

    change focus node  utils added

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,1 +1,10 @@
-class Utils {}
+import 'package:flutter/material.dart';
+
+class Utils {
+  // function to change the focus from the current textfield to another with keyboard
+  static void fieldFocusChange(
+      BuildContext context, FocusNode current, FocusNode nextFocus) {
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+}
