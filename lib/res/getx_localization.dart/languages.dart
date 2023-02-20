fe11d5ec9d9c47318c2f721f74ec3cd3ec3a12a4
Ansh Malik <anshmalik279@gmail.com>
2023-02-20 12:04:08 +0530

    some common Components created

--- lib/res/getx_localization.dart/languages.dart
+++ lib/res/getx_localization.dart/languages.dart
@@ -7,11 +7,17 @@ class Languages extends Translations {
           'email_hint': 'Enter email',
           'internet_exception':
               'We are unable to show result.\nPlease check yout network conection',
+          'general_exception':
+              'We are unable to process ur request\nPlease try again',
+          'retry': 'Retry',
         },
         'hi_ID': {
           'email_hint': ' ईमेल दर्ज करें ',
           'internet_exception':
-              'हम परिणाम दिखाने में असमर्थ हैं।\nकृपया अपने नेटवर्क कनेक्शन की जाँच करें'
+              'हम परिणाम दिखाने में असमर्थ हैं।\nकृपया अपने नेटवर्क कनेक्शन की जाँच करें',
+          'general_exception':
+              'हम आपके अनुरोध को संसाधित करने में असमर्थ हैं।\nकृपया पुनः प्रयास करें',
+          'retry': 'पुन: प्रयास करें'
         }
       };
 }
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mvvm_mvc_with_getx/data/app_exception.dart';
+import 'package:mvvm_mvc_with_getx/res/components/general_exception_widget.dart';
+import 'package:mvvm_mvc_with_getx/res/components/internet_exception_widget.dart';
+import 'package:mvvm_mvc_with_getx/res/components/round_button.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -12,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('email_hint'.tr)),
-    );
+        appBar: AppBar(title: Text('email_hint'.tr)),
+        body: RoundeButton(title: "helo", onPress: (() {})));
   }
 }
