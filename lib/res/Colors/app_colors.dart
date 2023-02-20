e9de1edd00a2930446e7dc0ef107ddf95de7b7c0
Ansh Malik <anshmalik279@gmail.com>
2023-02-20 10:37:55 +0530

    updated color class

--- lib/res/Colors/app_colors.dart
+++ lib/res/Colors/app_colors.dart
@@ -2,5 +2,18 @@ import 'dart:ui';
 
 class AppColor {
   static const Color blackColor = Color(0xff000000);
+
   static const Color wightColor = Color(0xffffffff);
+
+  static const Color redColor = Color(0xffFC3F5B);
+
+  static const Color primaryColor = Color(0xff01b1C9);
+
+  static const Color primaryButtonColor = Color(0xff01b1C9);
+
+  static const Color secondaryButtonColor = Color(0xffFC3F5B);
+
+  static const Color primaryTextColor = Color(0xff000000);
+
+  static const Color secondaryTextColor = Color(0xff444648);
 }
