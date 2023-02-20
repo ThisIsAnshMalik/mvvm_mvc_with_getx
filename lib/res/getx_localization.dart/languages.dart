b7751a4a25cbbc03b54f05ce8e7b04287ad93844
Ansh Malik <anshmalik279@gmail.com>
2023-02-20 10:41:26 +0530

    langvage class updated

--- lib/res/getx_localization.dart/languages.dart
+++ lib/res/getx_localization.dart/languages.dart
@@ -3,7 +3,15 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 class Languages extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-        'en_US': {'email_hint': 'Enter email'},
-        'hi_ID': {'email_hint': ' ईमेल दर्ज करें '}
+        'en_US': {
+          'email_hint': 'Enter email',
+          'internet_exception':
+              'We are unable to show result.\nPlease check yout network conection',
+        },
+        'hi_ID': {
+          'email_hint': ' ईमेल दर्ज करें ',
+          'internet_exception':
+              'हम परिणाम दिखाने में असमर्थ हैं।\nकृपया अपने नेटवर्क कनेक्शन की जाँच करें'
+        }
       };
 }
